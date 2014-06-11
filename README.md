193e6101ce0720dcd6d2bef7221439e0f425effa
Doug McInnes <doug@dougmcinnes.com>
2014-06-10 20:59:25 -0700

    Initial add from the MagicMirror codebase
    https://github.com/MichMich/MagicMirror
    Didn't want to do a fork since I'm going to change it considerably. This
    original codebase is a good starting point.

---
+++ README.md
@@ -0,0 +1,5 @@
+MirrorMirror
+===========
+
+Largely based on Michael Teeuw's [MagicMirror codebase](https://github.com/MichMich/MagicMirror).
+More details as his [blog](http://michaelteeuw.nl/tagged/magicmirror).
---
+++ js/config.js
@@ -0,0 +1,23 @@
+// for navigator language
+var lang = window.navigator.language;
+// you can change the language
+//var lang = 'en';
+
+//change weather params here:
+var weatherParams = {
+    'q':'Portland,OR',
+    'units':'imperial',
+    'lang':lang
+};
+
+// compliments:
+var compliments = [
+            'Hey, handsome!',
+            'Hi, sexy!',
+            'Hello, beauty!',
+            'You look sexy!',
+            'Wow, you look hot!',
+            'Looking good today!',
+            'You look nice!',
+            'Enjoy your day!'
+        ];

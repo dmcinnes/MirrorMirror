445f30b4135421e03dc38374011ba51bb862a179
Doug McInnes <doug@dougmcinnes.com>
2014-07-18 19:58:23 -0700

    prettify the controls

--- js/controls.js
+++ js/controls.js
@@ -1,12 +1,16 @@
 $(function () {
   if (/ip(hone|od|ad)/i.test(navigator.userAgent) ||
       window.location.hash === '#controls') {
-    var shutdown = $('<input/>', {type: 'button', value: 'Shut Down'});
+    var shutdown = $('<a/>', {href:'#'}).addClass('command').text('Shut Down');
     shutdown.click(function (e) {
-      Connection.send('shutdown');
+      e.preventDefault();
+      if (window.confirm('Are you sure?')) {
+        Connection.send('shutdown');
+      }
     });
-    var refresh = $('<input/>', {type: 'button', value: 'Refresh'});
+    var refresh = $('<a/>', {href:'#'}).addClass('command').text('Refresh');
     refresh.click(function (e) {
+      e.preventDefault();
       Connection.send('refresh');
     });
     $('.bottom').append(shutdown, refresh);

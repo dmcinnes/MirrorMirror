b10eee5f294d6ee48ffbca05b4cd568ef94b9637
Doug McInnes <doug@dougmcinnes.com>
2014-07-18 21:22:14 -0700

    only update time when the minutes change

--- js/time.js
+++ js/time.js
@@ -1,19 +1,22 @@
 $(function() {
 
+  var now;
+
   var $elements = $('.date,.time').hide();
 
   var updateTime = function () {
-    var now = moment();
-    var date = now.format('LLLL').split(' ', 4);
-    date = date[0] + ' ' + date[1] + ' ' + date[2] + ' ' + date[3];
+    var newNow = moment();
+    if (!now || now.minutes() !== newNow.minutes()) {
+      now = newNow;
+      var date = now.format('LLLL').split(' ', 4);
+      date = date[0] + ' ' + date[1] + ' ' + date[2] + ' ' + date[3];
 
-    $('.date').html(date);
-    $('.time').html(now.format('h') + ':' + now.format('mm') + '<span class="ampm">'+now.format('A')+'</span>');
+      $('.date').html(date);
+      $('.time').html(now.format('h') + ':' + now.format('mm') + '<span class="ampm">'+now.format('A')+'</span>');
+    }
   };
 
-  setTimeout(function() {
-    updateTime();
-  }, 1000);
+  setInterval(updateTime, 1000);
 
   Mirror.listen({
     show: function () {
